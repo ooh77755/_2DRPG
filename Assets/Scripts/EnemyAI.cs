358656328ea8006b9189147cd778dc39f8ee36bd
ooh77755 <86021255+ooh77755@users.noreply.github.com>
2023-04-14 12:37:28 +0100

    Pre-Challenge: imported Enemy & began setup re: EnemyAI (assigning random xPos & yPos every 2secs)

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private enum State
+    {
+        Roaming
+    }
+
+    State state;
+    EnemyPathfinding enemyPath;
+
+    private void Awake()
+    {
+        state = State.Roaming;
+        enemyPath = GetComponent<EnemyPathfinding>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(RoamingRoutine());
+    }
+
+    IEnumerator RoamingRoutine()
+    {
+        while(state == State.Roaming)
+        {
+            Vector2 roamPos = GetRoamingPos();
+            print(roamPos);
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    Vector2 GetRoamingPos()
+    {
+        return new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+    }
+}
