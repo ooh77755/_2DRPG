04e0f4b3b4b4fa2505b05881294128013aac8bdc
alan.stewart@chi.ac.uk <alan.stewart@chi.ac.uk>
2024-09-19 09:27:30 +0100

    Stephen solution: dash functionality (inc. trail fx)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,13 +9,18 @@ public class PlayerController : MonoBehaviour
     public static PlayerController Instance;
 
     [SerializeField] float moveSpeed = 1f;
+    float dashSpeed = 5f;
+    float dashTime = .5f;
+    float dashCooldown = .75f;
 
+    [SerializeField] TrailRenderer tR;
     PlayerControls pC;
     Vector2 movement;
     Rigidbody2D rB;
     Animator anim;
 
     bool facingLeft = false;
+    bool isDashing = false;
 
     private void Awake()
     {
@@ -23,6 +28,11 @@ public class PlayerController : MonoBehaviour
         pC = new PlayerControls();
         rB = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        pC.Combat.Dash.performed += _ => Dash();
     }
 
     private void OnEnable()
@@ -67,5 +77,25 @@ public class PlayerController : MonoBehaviour
             transform.localScale = new Vector2(1f, 1f);
             FacingLeft = false;
         }
+    }
+
+    void Dash()
+    {
+        if(!isDashing)
+        {
+            isDashing = true;
+            moveSpeed += dashSpeed;
+            tR.emitting = true;
+            StartCoroutine(EndDash());
+        }
+    }
+
+    IEnumerator EndDash()
+    {
+        yield return new WaitForSeconds(dashTime);
+        tR.emitting = false;
+        moveSpeed /= dashSpeed;
+        yield return new WaitForSeconds(dashCooldown);
+        isDashing = false;
     }
 }
